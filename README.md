ec74406297669bd8eaa18e7cd88c0dde7abd1e06
WalkerTexas-coder <austinsrwalker@gmail.com>
2022-09-22 10:13:15 -0700

    Cat views deleted, seeds added, CORS added

--- README.md
+++ README.md
@@ -1,24 +1,32 @@
-# README
+# Rails Set up
+$ rails new cat-tinder-backend -d postgresql -T
+$ cd cat-tinder-backend
+$ rails db:create
+$ bundle add rspec-rails
+$ rails generate rspec:install
+Add the remote from your GitHub classroom repository
+Create a default branch (main)
+Make an initial commit to the repository
+$ rails server
+$ rails generate resource Cat name:string age:integer enjoys:text image:text
+$ rails db:migrate
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+# Rails Seeds
+```ruby
+ cats = [{
+    name: "Clawdia",
+    age: 15,
+    enjoys: "Shopping",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0B4qo_jSCNLYoEGOwWf_oJ5KcZc2MPz7a-Q&usqp=CAU"
+  }, 
+]
 
-Things you may want to cover:
+cats.each do |indiv_cat|
+  Cat.create indiv_cat
+  p "#{indiv_cat} was added to the database"
+end
+```
+ $ rails db:seed
 
-* Ruby version
+# CORS
 
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+cats = [
+  {
+    name: "Mosey",
+    age: 7,
+    enjoys: "showing up in places he is not expected",
+    image: "https://people.com/thmb/TOWzG1I45R7tb-cnu8eQmLlW1go=/900x0/filters:no_upscale():max_bytes(150000):strip_icc():gifv():focal(659x344:661x346):format(webp)/Mostaccioli-01-831c65474751439d8f1b72eee9be76a5.jpg"
+  }, 
+  {
+    name: "Kelly",
+    age: 23,
+    enjoys: "Aspiring souncloud artists",
+    image: "https://ih1.redbubble.net/image.690761378.7456/flat,750x,075,f-pad,750x1000,f8f8f8.jpg"
+  }, 
+  {
+    name: "Puss in Boots",
+    age: 4,
+    enjoys: "Tom cat things",
+    image: "https://upload.wikimedia.org/wikipedia/commons/d/dc/Grumpy_Cat_%2814556024763%29_%28cropped%29.jpg"
+  }, 
+  {
+    name: "Clawdia",
+    age: 15,
+    enjoys: "Shopping",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0B4qo_jSCNLYoEGOwWf_oJ5KcZc2MPz7a-Q&usqp=CAU"
+  }, 
+]
+
+cats.each do |indiv_cat|
+  Cat.create indiv_cat
+  p "#{indiv_cat} was added to the database"
+end
+
+p "Database status and confirmation"
+p Cat.all 
